7d32603bcc4778f4ec4f12af86a17786ddc109dc
Matt <mattjarek@gmail.com>
2025-07-04 12:29:35 -0400

    Updated logic for the game scabble

--- js/script.js
+++ js/script.js
@@ -73,10 +73,21 @@ class TileBag {
         }
 
     }
+    getScoreValue(letter){
+        var tile = this[letter];
+        if (!tile) {
+            console.log(`No such tile: ${letter}`);
+            return null;
+        }
+        else{
+            return tile.value;
+        }
+    }
 }
 
 const bag = new TileBag(pieces);
 var placeAnywhere = true;
+var score = 0;
 
 $(function () {
     makeBoard();
@@ -91,13 +102,19 @@ $(function () {
             blockCol = parseInt(this.dataset.col)
             blockRow = parseInt(this.dataset.row)
             enableSurroundingTiles(blockRow, blockCol);
-            console.log(`${this.dataset.col}`);
-
+            nScore =(tallyScore(score));
+            $('.score').text(`${nScore}`);
         }
     });
-    $(".draggable").each(function(){
-        
-    })
+    $(".droppable-trash").droppable({
+        disabled: false,
+        drop: function (event, ui) {
+            const $droppable = $(this);
+            const $draggable = ui.helper;
+            $draggable.data("wasDropped", true);
+            $droppable.data("hasTile", true);
+        }
+    });
     draggable("#one");
     draggable("#two");
     draggable("#three");
@@ -108,90 +125,58 @@ $(function () {
 });
 
 function enableSurroundingTiles(centerRow, centerCol) {
-
-}
-
-function tallyScore() {
-    sum = 0;
-    $(".droppable").filter(function () {
-        return $(this).data("hasTile");
-    }).each(function () {
-        const $square = $(this);
-        sum = sum + $square.data.
-            placedTiles.push({
-                row: parseInt($square.data("row")),
-                col: parseInt($square.data("col")),
-                letter: $square.find("img").attr("alt")  // assuming your tile image's alt is the letter
-            });
+    $(".droppable").each(function () {
+        $(this).droppable("option", "disabled", true).addClass("droppable-disabled");
     });
-}
-
-
-function enableSurroundingTilesv1(centerRow, centerCol) {
-
-    let placedTiles = [];
-
     $(".droppable").filter(function () {
-        return $(this).data("hasTile");
-    }).each(function () {
-        const $square = $(this);
-        placedTiles.push({
-            row: parseInt($square.data("row")),
-            col: parseInt($square.data("col")),
-            letter: $square.find("img").attr("alt")  // assuming your tile image's alt is the letter
-        });
-    });
-
-    console.log(placedTiles);
-    // $(".droppable").each(function () {
-    //     $(this).droppable("option", "disabled", true).addClass("droppable-disabled");
-    // });
-
-    $("droppable").filter(function () {
         return $(this).data("hasTile");
     }).each(function () {
         const $square = $(this);
         const row = parseInt($square.data("row"));
         const col = parseInt($square.data("col"));
         const neighbors = [
-            [row - 1, col],
-            [row + 1, col],
-            [row, col - 1],
-            [row, col + 1],
         ];
+        horizontal = $(`.droppable[data-row='${row}'][data-col='${col-1}']`).data("hasTile");
+        horizontal = $(`.droppable[data-row='${row}'][data-col='${col+1}']`).data("hasTile") || horizontal;
+        vertical = $(`.droppable[data-row='${row-1}'][data-col='${col}']`).data("hasTile");
+        vertical = $(`.droppable[data-row='${row+1}'][data-col='${col}']`).data("hasTile") || vertical;
+        if(horizontal && vertical){
+            //Add later
+        }
+        else{
+            if(!horizontal){
+                neighbors.push([row - 1, col]);
+                neighbors.push([row + 1, col]);
+            }
+            if(!vertical){
+                neighbors.push([row, col - 1]);
+                neighbors.push([row, col + 1]);
+            }
+        }
+        console.log(`Vertical: ${!vertical}, Horizontal: ${!horizontal}`)
+        console.log(`${$(this).data("hasTile")}`)
         neighbors.forEach(([nRow, nCol]) => {
             const selector = `.droppable[data-row='${nRow}'][data-col='${nCol}']`;
             const $neighbor = $(selector);
             $neighbor.droppable("option", "disabled", false).removeClass("droppable-disabled");
-            // Only enable empty, unfilled squares
-            // if ($neighbor.length && !$neighbor.data("hasTile")) {
-            //     $neighbor.droppable("option", "disabled", false).removeClass("droppable-disabled");
-            // }
         });
-
     });
-    // $("data(has").each(function(){
-    //     const $square = $(this);
-    //     const row = parseInt($square.data("row"));
-    //     const col = parseInt($square.data("col"));
-    //     const rowDiff = Math.abs(row - centerRow);
-    //     const colDiff = Math.abs(col - centerCol);
-    //     const isSurrounding = ((rowDiff == 1 && colDiff == 0)||(rowDiff == 0 && colDiff == 1)) && !$square.data("hasTile");
-    //     $(".droppable").each(function(){
-    //         const $checkSquare = $(this);
-    //         const checkRow = parseInt($square.data("row"));
-    //         const checkCol = parseInt($square.data("col"));
-    //         const checkrowDiff = Math.abs(row - centerRow);
-    //         const checkcolDiff = Math.abs(col - centerCol);
-    //         // if(rowDiff)
-    //     });
-    //     if(isSurrounding){
-    //         $square.droppable("option", "disabled", false).removeClass("droppable-disabled");
-    //     }
-    //     else{
-    //         $square.droppable("option", "disabled", true).addClass("droppable-disabled");
-    //     }
-    // })
+}
+
+function tallyScore(score = 0) {
+    placedTiles = []
+    wordMult = 1;
+    sum = score;
+    $(".droppable").filter(function () {
+        return $(this).data("hasTile");
+    }).each(function () {
+        const $square = $(this);
+        // sum = sum + $square.data.
+            placedTiles.push($square.find("img").attr("alt"))  // assuming your tile image's alt is the letter);
+            sum += bag.getScoreValue($square.find("img").attr("alt")) * $square.data("letterMult");
+            wordMult *= $square.data("wordMult");
+    });
+    return(sum*wordMult)
 }
 
 function draggable(id) {
@@ -201,7 +186,8 @@ function draggable(id) {
         },
         stop: function (event, ui) {
             if (ui.helper.data("wasDropped")) {
-                $(this).find(".tile").hide();
+                $(this).find(".tile").attr("src", "graphics_data/Solid/black_square.png");
+                $(this).draggable("option", "disabled", true);
             }
             $(this).css({
                 top: 0,
@@ -210,7 +196,11 @@ function draggable(id) {
 
         }
     });
-    $(id).find(".tile").html(makeTileImageHtml());
+    image = $(id).find(".tile").html(makeTileImageHtml());
+    tile = bag.drawTile();
+    image.attr("src", tile.image);
+    image.attr("alt", tile.letter);
+    image.addClass("tile-piece")
 }
 
 function makeTileImageHtml() {
@@ -227,72 +217,72 @@ function makeBoard() {
     const boardHeight = 15;
     const boardBody = document.getElementById("board");
     const tripple_word = [
-        [0,0],
-        [0,7],
-        [0,14],
-        [7,0],
-        [7,14],
-        [14,0],
-        [14,7],
-        [14,14]
+        [0, 0],
+        [0, 7],
+        [0, 14],
+        [7, 0],
+        [7, 14],
+        [14, 0],
+        [14, 7],
+        [14, 14]
     ]
     const double_letter = [
-        [0,3],
-        [0,11],
-        [2,6],
-        [2,8],
-        [3,0],
-        [3,7],
-        [3,14],
-        [6,2],
-        [6,6],
-        [6,8],
-        [6,12],
-        [7,3],
-        [7,11],
-        [14,3],
-        [14,11],
-        [12,6],
-        [12,8],
-        [11,0],
-        [11,7],
-        [11,14],
-        [8,2],
-        [8,6],
-        [8,8],
-        [8,12]
+        [0, 3],
+        [0, 11],
+        [2, 6],
+        [2, 8],
+        [3, 0],
+        [3, 7],
+        [3, 14],
+        [6, 2],
+        [6, 6],
+        [6, 8],
+        [6, 12],
+        [7, 3],
+        [7, 11],
+        [14, 3],
+        [14, 11],
+        [12, 6],
+        [12, 8],
+        [11, 0],
+        [11, 7],
+        [11, 14],
+        [8, 2],
+        [8, 6],
+        [8, 8],
+        [8, 12]
     ]
     const tripple_letter = [
-        [1,5],
-        [1,9],
-        [5,1],
-        [5,5],
-        [5,9],
-        [5,13],
-        [13,5],
-        [13,9],
-        [9,1],
-        [9,5],
-        [9,9],
-        [9,13]
+        [1, 5],
+        [1, 9],
+        [5, 1],
+        [5, 5],
+        [5, 9],
+        [5, 13],
+        [13, 5],
+        [13, 9],
+        [9, 1],
+        [9, 5],
+        [9, 9],
+        [9, 13]
     ]
     const double_word = [
-        [1,1],
-        [1,13],
-        [2,2],
-        [2,12],
-        [3,3],
-        [3,11],
-        [4,4],
-        [4,10],
-        [13,1],
-        [13,13],
-        [12,2],
-        [12,12],
-        [11,3],
-        [11,11],
-        [10,4],
-        [10,10],
+        [1, 1],
+        [1, 13],
+        [2, 2],
+        [2, 12],
+        [3, 3],
+        [3, 11],
+        [4, 4],
+        [4, 10],
+        [13, 1],
+        [13, 13],
+        [12, 2],
+        [12, 12],
+        [11, 3],
+        [11, 11],
+        [10, 4],
+        [10, 10],
     ]
 
     for (let row = 0; row < boardHeight; row++) {
@@ -302,29 +292,36 @@ function makeBoard() {
             td.classList.add("droppable")
             td.dataset.row = row;
             td.dataset.col = col;
+            td.dataset.wordMult = 1;
+            td.dataset.letterMult = 1;
+            td.dataset.hasTile = false;
             td.innerHTML = `<img class="board-tile" src="graphics_data/Solid/tile.png" alt="Drop Here">`;
-            for (let i =0; i<tripple_word.length;i++){
-                if((row==tripple_word[i][0]) && (col==tripple_word[i][1])){
-                    td.innerHTML = `<img class="board-tile" src="graphics_data/Solid/letter_T.png" alt="T">`;
+            for (let i = 0; i < tripple_word.length; i++) {
+                if ((row == tripple_word[i][0]) && (col == tripple_word[i][1])) {
+                    td.innerHTML = `<img class="board-tile" src="graphics_data/Solid/letter_TW.png" alt="T">`;
+            td.dataset.wordMult = 3;
                 }
             }
-            for (let i =0; i<double_letter.length;i++){
-                if((row==double_letter[i][0]) && (col==double_letter[i][1])){
-                    td.innerHTML = `<img class="board-tile" src="graphics_data/Solid/letter_D.png" alt="D">`;
+            for (let i = 0; i < double_letter.length; i++) {
+                if ((row == double_letter[i][0]) && (col == double_letter[i][1])) {
+                    td.innerHTML = `<img class="board-tile" src="graphics_data/Solid/letter_DL.png" alt="D">`;
+            td.dataset.letterMult = 2;
                 }
             }
-            for (let i =0; i<tripple_letter.length;i++){
-                if((row==tripple_letter[i][0]) && (col==tripple_letter[i][1])){
-                    td.innerHTML = `<img class="board-tile" src="graphics_data/Solid/letter_L.png" alt="T">`;
+            for (let i = 0; i < tripple_letter.length; i++) {
+                if ((row == tripple_letter[i][0]) && (col == tripple_letter[i][1])) {
+                    td.innerHTML = `<img class="board-tile" src="graphics_data/Solid/letter_TL.png" alt="T">`;
+            td.dataset.letterMult = 3;
                 }
             }
-            for (let i =0; i<double_word.length;i++){
-                if((row==double_word[i][0]) && (col==double_word[i][1])){
-                    td.innerHTML = `<img class="board-tile" src="graphics_data/Solid/letter_S.png" alt="S">`;
+            for (let i = 0; i < double_word.length; i++) {
+                if ((row == double_word[i][0]) && (col == double_word[i][1])) {
+                    td.innerHTML = `<img class="board-tile" src="graphics_data/Solid/letter_DW.png" alt="S">`;
+            td.dataset.wordMult = 2;
                 }
             }
-            if((row == 7)&&(col==7)){
-                    td.innerHTML = `<img class="board-tile" src="graphics_data/Solid/letter__.png" alt="_">`;
+            if ((row == 7) && (col == 7)) {
+                td.innerHTML = `<img class="board-tile" src="graphics_data/Solid/letter_start.png" alt="_">`;
             }
             tr.appendChild(td);
         }
